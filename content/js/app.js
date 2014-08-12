3dba030c7753d90bef3d33283dd9b98592237013
SimonGodbout <simongodbout@scolab.com>
2014-08-12 13:26:00 -0400

    added images for porfolio

---
+++ content/js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Simon Godbout on 2014-07-02.
+ */
+var app = angular.module("website", []);
+app.controller("pageController", function(){
+    var currentPage = 0;
+    var switchPage = function(page){
+
+    }
+})
